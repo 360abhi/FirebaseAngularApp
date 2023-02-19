8e4800e6094bc4e27918335880978e502decb711
Abhishek Chhawari <abhishekch360@gmail.com>
2023-02-19 16:35:08 +0530

    update operation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,61 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FirebaseService } from '../Services/firebase.service';
-import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [FirebaseService]
- 
+  providers: [FirebaseService],
 })
-export class HomeComponent  implements OnInit{
-  constructor(public _firebase:FirebaseService) {}
+export class HomeComponent implements OnInit {
+  constructor(public _firebase: FirebaseService) {}
 
   ngOnInit(): void {
-      this.onFetchProducts();
+    this.onFetchProducts();
   }
 
   dataTitle = this._firebase.fetchTitle();
-
-  fetching:boolean = false;
-  uname = '';
-  uid ='';
-  uprice  = 0;
-  products = [
-    // {
-    //   id: 'p1',
-    //   name: 'Mobile',
-    //   price: 5000,
-    // },
-    // {
-    //   id: 'p2',
-    //   name: 'TV',
-    //   price: 55000,
-    // },
-    // {
-    //   id: 'p3',
-    //   name: 'Flip Mobile',
-    //   price: 24000,
-    // },
-  ];
+  fetching: boolean = false;
+  products;
+  // {
+  //   id: 'p1',
+  //   name: 'Mobile',
+  //   price: 5000,
+  // },
+  // {
+  //   id: 'p2',
+  //   name: 'TV',
+  //   price: 55000,
+  // },
+  // {
+  //   id: 'p3',
+  //   name: 'Flip Mobile',
+  //   price: 24000,
+  // },
 
   addProduct(id, name, price): void {
-    if (id.length > 0 && name.length > 0 && price> 0) {
-      this.products.push({
+    if (this.editmode) {
+      this.products[this.editIndex] = {
         id: id,
         name: name,
         price: price,
-      });
+      };
+      this.editmode = false;
+      this.id.nativeElement.value = '';
+      this.name.nativeElement.value = '';
+      this.price.nativeElement.value = '';
+    } else {
+      if (id.length > 0 && name.length > 0 && price > 0) {
+        this.products.push({
+          id: id,
+          name: name,
+          price: price,
+        });
+      }
     }
-    this.uid = '';
-    this.uname ='';
-    this.uprice = 0;
+    this.saveProduct();
   }
 
   onRemove(index: number): void {
-    this.products.splice(index,1);
+    this.products.splice(index, 1);
     this.saveProduct();
   }
 
@@ -63,21 +65,34 @@ export class HomeComponent  implements OnInit{
     this._firebase.saveProducts(this.products).subscribe(
       (response) => console.log(response),
       (err) => console.log(err)
-    )
+    );
   }
 
-  onFetchProducts(){
+  onFetchProducts() {
     this.fetching = true;
-    this._firebase.fetchProducts()
-    .subscribe(
-      (response) =>{
+    this._firebase.fetchProducts().subscribe(
+      (response) => {
         // console.log(response)
-        const data = JSON.stringify(response);
-        this.products = JSON.parse(data);
+        // const data = JSON.stringify(response);
+        // this.products = JSON.parse(data);
+        this.products = response;
         this.fetching = false;
-      } 
-      ,
+      },
       (err) => console.log(err)
-    )
+    );
+  }
+
+  @ViewChild('id') id: ElementRef;
+  @ViewChild('name') name: ElementRef;
+  @ViewChild('price') price: ElementRef;
+  editmode: boolean = false;
+  editIndex: number;
+
+  onEditProduct(index: number): void {
+    this.editIndex = index;
+    this.editmode = true;
+    this.id.nativeElement.value = this.products[index].id;
+    this.name.nativeElement.value = this.products[index].name;
+    this.price.nativeElement.value = this.products[index].price;
   }
 }
